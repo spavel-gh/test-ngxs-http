365cc9a78b4127e97018b78ca7687f41c9298628
spavel-gh <spavel.mail@gmail.com>
2021-03-19 18:20:12 +0200

    add http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription, Observable } from 'rxjs';
+
+interface DataModele {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-ngxs';
+
+  data?: DataModele;
+  private subscription: Subscription = new Subscription();
+
+  constructor(
+    private httpClient: HttpClient,
+  ){}
+
+
+  getData(): Observable<DataModele> {
+    return this.httpClient.get<DataModele>('https://jsonplaceholder.typicode.com/todos/1').pipe();
+  }
+
+  updateData(): void {
+    this.subscription.add(this.getData().subscribe((newData: DataModele) => {
+      this.data = newData;
+      console.log(this.data);
+    }));
+  }
+
 }
