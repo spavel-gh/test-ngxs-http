eda8521cf1ea739d1d962093337f020dbf2e4987
spavel-gh <spavel.mail@gmail.com>
2021-03-19 18:23:44 +0200

    install ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    NgxsModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
